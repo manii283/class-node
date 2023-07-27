70e177c9a94cf946e38fae8f3ccdf4a2f098f471
MANISHA NAYAK <nayakmanisha515@gmail.com>
2023-07-27 23:15:35 +0530

    node file system

---
+++ fileSystem/index.js
@@ -0,0 +1,22 @@
+var fs = require("fs");
+
+//create file
+fs.writeFile("read.txt", "today is awesome day", (err) => {
+  console.log("file is create");
+  console.log(err);
+});
+
+// append file ( add data)
+fs.appendFile("read.txt", "manishaaaaaaaaaaaaaaaaaa", (err) => {
+  console.log("tast complete");
+});
+
+fs.open('read.txt', 'w', (err) => {
+  if (err) throw err;
+  console.log('Saved!');
+});
+
+//read data 
+fs.readFile('read.txt','utf-8', (err, data) => {
+  console.log(data);
+} )
