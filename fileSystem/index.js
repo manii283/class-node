7f04322f0a52270c65fad80ef643259cebb20009
MANISHA NAYAK <nayakmanisha515@gmail.com>
2023-07-27 23:48:49 +0530

    url modul

--- fileSystem/index.js
+++ fileSystem/index.js
@@ -11,12 +11,23 @@ fs.appendFile("read.txt", "manishaaaaaaaaaaaaaaaaaa", (err) => {
   console.log("tast complete");
 });
 
-fs.open('read.txt', 'w', (err) => {
+fs.open("read.txt", "w", (err) => {
   if (err) throw err;
-  console.log('Saved!');
+  console.log("Saved!");
 });
 
-//read data 
-fs.readFile('read.txt','utf-8', (err, data) => {
+//read data
+fs.readFile("read.txt", "utf-8", (err, data) => {
   console.log(data);
-} )
+});
+
+fs.rename('read.txt', 'myrenamedfile.txt', function (err) {
+  if (err) throw err;
+  console.log('File Renamed!');
+});
+
+//Delete
+// fs.unlink("read.txt", (err) => {
+//   if (err) throw err;
+//   console.log("File deleted!");
+// });
