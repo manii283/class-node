69e1da671dc29b71b073a7e422e0a67e885fc579
MANISHA NAYAK <nayakmanisha515@gmail.com>
2023-07-31 02:41:13 +0530

    drop only if exist

--- mysql/drop-table/index.js
+++ mysql/drop-table/index.js
@@ -1,17 +1,26 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "mydb"
+  database: "mydb",
 });
 
-con.connect(function(err) {
+// con.connect(function(err) {
+//   if (err) throw err;
+//   var sql = "DROP TABLE customers";
+//   con.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log("Table deleted");
+//   });
+// });
+
+con.connect(function (err) {
   if (err) throw err;
-  var sql = "DROP TABLE customers";
+  var sql = "DROP TABLE IF EXISTS customers";
   con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Table deleted");
+    console.log(result);
   });
 });
