af3bfd8a4b1b39741a384a5187a81800ac6b2a22
MANISHA NAYAK <nayakmanisha515@gmail.com>
2023-07-31 02:52:46 +0530

    update table

--- mysql/create-table/index.js
+++ mysql/create-table/index.js
@@ -7,16 +7,26 @@ var con = mysql.createConnection({
   database: "mydb",
 });
 
-con.connect(function (err) {
+con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  var sql =
-    "CREATE TABLE books (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))";
+  var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table created");
   });
 });
+
+// con.connect(function (err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   var sql =
+//     "CREATE TABLE books (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))";
+//   con.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log("Table created");
+//   });
+// });
 
 //-----table already exist
 
