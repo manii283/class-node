b1605abaf5d720ee9f6da3309b6421d351bc8131
MANISHA NAYAK <nayakmanisha515@gmail.com>
2023-07-31 14:44:51 +0530

    starting from another position

--- mysql/mysql-limit/index.js
+++ mysql/mysql-limit/index.js
@@ -7,11 +7,21 @@ var con = mysql.createConnection({
   database: "mydb"
 });
 
+// con.connect(function(err) {
+//   if (err) throw err;
+//   var sql = "SELECT * FROM customers LIMIT 5";
+//   con.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log(result);
+//   });
+// });
+
+
 con.connect(function(err) {
-  if (err) throw err;
-  var sql = "SELECT * FROM customers LIMIT 5";
-  con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log(result);
+    var sql = "SELECT * FROM customers LIMIT 5 OFFSET 4";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
   });
-});
